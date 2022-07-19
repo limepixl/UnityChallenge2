34c45c6f6fac6f36c47f59f35eb2cf6b8a03ea6d
Stefan Ivanovski <sivanovski.dev@gmail.com>
2022-07-19 15:24:38 +0200

    Bonus: Variable spawn rate of balls

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,7 +11,7 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = 3.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +20,15 @@ public class SpawnManagerX : MonoBehaviour
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
+        // The repeat delay is 3 seconds, and we can delay further 
+        // by n seconds, where n is [0,2] so the max repeat rate
+        // is 5 seconds and the min repeat rate is 3 seconds.
+        float delay = Random.Range(0.0f, 2.0f);
+        Delay(delay);
+        Debug.Log("Ball spawning delayed by " + delay + " seconds. Total time since last spawn: " + (delay + spawnInterval));
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
@@ -31,6 +38,11 @@ public class SpawnManagerX : MonoBehaviour
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+    IEnumerable Delay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
     }
 
 }
