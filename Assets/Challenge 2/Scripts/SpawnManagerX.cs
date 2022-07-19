9c2e8d60bc9d130905f592075d733c4a02e4f5b8
Stefan Ivanovski <sivanovski.dev@gmail.com>
2022-07-19 15:11:47 +0200

    Use random ball prefab instead of only the blue one

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -25,8 +25,12 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        // Random index in [0, numBallPrefabs - 1]
+        // (Using int overload with exclusive maximum range)
+        uint ballIndex = (uint)Random.Range(0, ballPrefabs.Length);
+
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
