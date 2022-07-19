3cc7693f2e5a9ec5d3be3284cee3fe88d2c37268
Stefan Ivanovski <sivanovski.dev@gmail.com>
2022-07-19 15:33:27 +0200

    Added minimum time between dog spawning

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float spawnRateInSeconds = 1.0f;
+    private float lastSpawnTime = 0;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        float currentTime = Time.time;
+        if (Input.GetKeyDown(KeyCode.Space) && (currentTime >= lastSpawnTime + spawnRateInSeconds || lastSpawnTime == 0.0f))
         {
+            lastSpawnTime = currentTime;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
